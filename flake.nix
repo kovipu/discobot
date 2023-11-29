cd034b3f25b2b7abe10611c39d19437abcafcbb7
Konsta Purtsi <konsta@arado.fi>
2023-11-29 21:54:05 +0200

    cabal init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "fleikki";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  nixConfig.bash-prompt-prefix = "[tunk]";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        haskellPackages = pkgs.haskell.packages.ghc945;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            haskellPackages.haskell-language-server
+            haskellPackages.cabal-install
+          ];
+        };
+      });
+}
